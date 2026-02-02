86b9338ec8327fcaedfb4148ccd4e93643be841d
Santiago Escobar <santiago.escobar30220@ucaldas.edu.co>
2026-02-02 12:18:55 -0500

    Add ExecuteJavaScript page and tests

---
+++ src/test/java/JavasScriptTests/JavaScriptTests.java
@@ -0,0 +1,36 @@
+package JavasScriptTests;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class JavaScriptTests extends BaseTests {
+
+    @Test
+    public void testScrollToTable(){
+        homePage.clickLargeAndDeepDom().scrollToTable();
+    }
+
+    @Test
+    public void testScrollToFifthParagraphsPresent(){
+        homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+    @Test
+    public void testAddMultipleAttributeToDropdown() {
+
+        var dropDownPage = homePage.clickDropDown();
+        dropDownPage.addMultipleAttribute();
+
+        var optionsToSelect = List.of("Option 1", "Option 2");
+        optionsToSelect.forEach(dropDownPage::selectFromDropDown);
+
+        var selectedOptions = dropDownPage.getSelectedOption();
+        assertTrue(selectedOptions.containsAll(optionsToSelect), "All options were not selected");
+        assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of selected items");
+    }
+}
